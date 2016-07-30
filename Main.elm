d60296c9f5b3317bf6b441e91a17f6ce87312b12
Daniel Wehner <dawehner@googlemail.com>
2016-07-30 22:28:49 +0200

    remove old stuff

--- Main.elm
+++ Main.elm
@@ -7,27 +7,6 @@ import Task
 import PostTypes exposing (..)
 import PostHttp exposing (postListDecoder, getPosts)
 
-model : BlogPost
-model = {
-  content = "This is some markdown
-* foo
-* bar
-* baz
-",
-  title = "My title"}
-
-model2 : PicturePost
-model2 = {
-  url = "http://hackenteer.com/wp-content/uploads/20130128-232104.jpg",
-  title = "Test streetart"}
-
-
-model3 : Post
-model3 = Blog model
-
-model4 : Post
-model4 = Picture model2
-
 type alias Model = PostList
 
 init : (Model, Cmd Msg)
